aa88a65e7d5eda545e67d003e4afd5b1ecfc49e4
Tito Ardimam Rahman <48154253+TitoRahman@users.noreply.github.com>
2025-09-11 20:59:08 +0700

    added M04 Advance question

--- CP SMP Advance/Warm Up 1/Answer/E.cpp
+++ CP SMP Advance/Warm Up 1/Answer/E.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 int main() {
-    // CODE
+    int t;
+    cin >> t;
+    for (size_t i = 1; i <= t; ++i) {
+        int n;
+        cin >> n;
+        int x[n];
+        for (size_t j = 0; j < n; j++)
+        {
+            cin >> x[j];
+        }
+        
+        int swap = 0;
+        bool possible = false;
+
+        
+    }
     return 0;
 }
